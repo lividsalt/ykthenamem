b4db6be15665946c0fe1e5f4c7d2152be182033d
lividsalt <101211795+lividsalt@users.noreply.github.com>
2022-05-20 14:01:10 -0700

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("hello!")
+name = input(str("what is your name?"))
+
+print("welcome " + name + " !")
+
+startprog = input("would you like to start the program? Y/N ")
+
+if startprog == "Y":
+  exec(open('').read())
